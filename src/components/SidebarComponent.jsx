768bc132909080b6f0399126ebbc745ff226d4cb
Metin KONUK <metinkonuk@hotmail.com>
2023-12-21 11:50:20 +0300

    configure the sidebar items for newly added algorithm routes

--- src/components/SidebarComponent.jsx
+++ src/components/SidebarComponent.jsx
@@ -17,10 +17,66 @@ import WolfIcon from '../assets/sidebarIcons/wolf.png'
 import SwarmIcon from '../assets/sidebarIcons/swarm.png'
 import MusicIcon from '../assets/sidebarIcons/music.png'
 import FlashIcon from '../assets/sidebarIcons/flash.png'
+import ArithmeticsIcon from '../assets/sidebarIcons/arithmetics.png'
+import EquilibriumIcon from '../assets/sidebarIcons/equilibrium.png'
+import FlaskIcon from '../assets/sidebarIcons/flask.png'
+import GazelleIcon from '../assets/sidebarIcons/gazelle.png'
+import GorillaIcon from '../assets/sidebarIcons/gorilla.png'
+import OrkaIcon from '../assets/sidebarIcons/orka.png'
+import VultureIcon from '../assets/sidebarIcons/vulture.png'
+
 import React from 'react'
 
 const SidebarComponent = ({ open, toggleSidebar }) => {
     const sidebarAlgorithmItems = [
+        {
+            primaryText: 'African Vultures Optimization',
+            secondaryText:
+                'Configure & add african vultures algorithm to execution list.',
+            to: '/avo',
+            image: (
+                <img
+                    src={VultureIcon}
+                    style={{ width: '24px', height: '24px' }}
+                />
+            ),
+        },
+        {
+            primaryText: 'Arithmetic Optimization Algorithm',
+            secondaryText:
+                'Configure & add arithmetic optimization algorithm to execution list.',
+            to: '/aoa',
+            image: (
+                <img
+                    src={ArithmeticsIcon}
+                    style={{ width: '24px', height: '24px' }}
+                />
+            ),
+        },
+        {
+            primaryText: 'Artificial Gorilla Troops Optimization',
+            secondaryText:
+                'Configure & add artificial gorilla troops optimization to execution list.',
+            to: '/agto',
+            image: (
+                <img
+                    src={GorillaIcon}
+                    style={{ width: '24px', height: '24px' }}
+                />
+            ),
+        },
+        {
+            primaryText: 'Equilibrium Optimization',
+            secondaryText:
+                'Configure & add equilibrium optimization to execution list.',
+            to: '/eo',
+            image: (
+                <img
+                    src={EquilibriumIcon}
+                    style={{ width: '24px', height: '24px' }}
+                />
+            ),
+        },
         {
             primaryText: 'Genetic Algorithm',
             secondaryText:
@@ -34,24 +90,57 @@ const SidebarComponent = ({ open, toggleSidebar }) => {
             ),
         },
         {
-            primaryText: 'Simulated Annealing Algorithm',
-            secondaryText:
-                'Configure & add simulated annealing algorithm to execution list.',
-            to: '/sa',
-            image: (
-                <img
-                    src={AnvilIcon}
-                    style={{ width: '24px', height: '24px' }}
-                />
-            ),
-        },
-        {
             primaryText: 'Grey Wolf Optimization',
             secondaryText:
                 'Configure & add grey wolf optimization to execution list.',
             to: '/gwo',
             image: (
                 <img src={WolfIcon} style={{ width: '24px', height: '24px' }} />
+            ),
+        },
+        {
+            primaryText: 'Harmony Search Algorithm',
+            secondaryText:
+                'Configure & add harmony search algorithm to execution list.',
+            to: '/hs',
+            image: (
+                <img
+                    src={MusicIcon}
+                    style={{ width: '24px', height: '24px' }}
+                />
+            ),
+        },
+        {
+            primaryText: 'Henry Gas Solubility Optimization',
+            secondaryText:
+                'Configure & add Henry Gas solubility optimization to execution list.',
+            to: '/hgso',
+            image: (
+                <img
+                    src={FlaskIcon}
+                    style={{ width: '24px', height: '24px' }}
+                />
+            ),
+        },
+        {
+            primaryText: 'Marine Predator Algorithm',
+            secondaryText:
+                'Configure & add marine predator algorithm to execution list.',
+            to: '/mpa',
+            image: (
+                <img src={OrkaIcon} style={{ width: '24px', height: '24px' }} />
+            ),
+        },
+        {
+            primaryText: 'Mountain Gazelle Optimization',
+            secondaryText:
+                'Configure & add mountain gazelle optimization to execution list.',
+            to: '/mgo',
+            image: (
+                <img
+                    src={GazelleIcon}
+                    style={{ width: '24px', height: '24px' }}
+                />
             ),
         },
         {
@@ -67,13 +156,13 @@ const SidebarComponent = ({ open, toggleSidebar }) => {
             ),
         },
         {
-            primaryText: 'Harmony Search Algorithm',
+            primaryText: 'Simulated Annealing Algorithm',
             secondaryText:
-                'Configure & add harmony search algorithm to execution list.',
-            to: '/hs',
+                'Configure & add simulated annealing algorithm to execution list.',
+            to: '/sa',
             image: (
                 <img
-                    src={MusicIcon}
+                    src={AnvilIcon}
                     style={{ width: '24px', height: '24px' }}
                 />
             ),
